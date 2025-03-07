a6469af0a855610520e99df4449290954a2aed02
April Joy U. Ytang <aj.ursal28@gmail.com>
2025-03-07 18:52:39 +0800

    in

--- Bank/Bank.java
+++ Bank/Bank.java
@@ -1,4 +1,55 @@
 package Bank;
+import Account.Account;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Bank {
+    private List<Account> accounts; //Store multiple accounts
+
+    public Bank()
+    {
+        this.accounts = new ArrayList<>();
+
+    }
+
+    public void addAccount(Account account)
+    {
+        accounts.add(account);
+        System.out.println("Account added: " + account.getAccountHolder());
+    }
+
+    //Find account by account number
+
+    public Account findAccount(String accountNumber)
+    {
+        for (Account acc : accounts)
+        {
+            if (acc.getAccountNumber().equals(accountNumber))
+            {
+                return acc;
+            }
+        }
+        return null; //account not found
+
+    }
+
+    //display all accounts
+    public void displayAccounts()
+    {
+        if (accounts.isEmpty())
+        {
+            System.out.println("No accounts available,");
+            return;
+        }
+        System.out.println("\nBank Account");
+        for (Account acc : accounts)
+        {
+            System.out.println(acc);
+        }
+    }
+
+
+
+
 }
--- Bank/BankLauncher.java
+++ Bank/BankLauncher.java
@@ -1,4 +1,28 @@
 package Bank;
 
+import Account.Account;
+
 public class BankLauncher {
+    public static void main (String[] args)
+    {
+        Bank myBank = new Bank();
+
+
+        //create accounts
+
+        Account acc1 = new Account("123456", "John Doe", 5000.0, "Savings");
+        Account acc2 = new Account("789012", "Alice Smith", 3000.0, "Checking");
+
+        //add accounts to bank
+        myBank.addAccount(acc1);
+        myBank.addAccount(acc2);
+
+        //perform transactions
+        acc1.deposit(1000);
+        acc1.withdraw(2000);
+        acc1.transfer(acc2, 1500);
+
+        //display all acounts
+        myBank.displayAccounts();
+    }
 }
