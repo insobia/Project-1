8d72c4ef6aed06abcd72176717c2ff20b125fbaa
razzienyt <timkamotycena@gmail.com>
2025-03-08 00:16:33 +0800

    in

---
+++ Bank/BankIdComparator.java
@@ -0,0 +1,9 @@
+package Bank;
+import java.util.Comparator;
+
+public class BankIdComparator implements Comparator<Bank> {
+    @Override
+    public int compare(Bank b1, Bank b2) {
+        return Integer.compare(b1.getID(), b2.getID());
+    }
+}
