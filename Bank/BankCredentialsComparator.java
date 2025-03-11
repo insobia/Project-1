ee88c4f80cada1fc5c924454fa8925715c434bb2
razzienyt <timkamotycena@gmail.com>
2025-03-11 18:18:11 +0800

    in

--- Bank/BankCredentialsComparator.java
+++ Bank/BankCredentialsComparator.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class BankCredentialsComparator implements Comparator<Bank> {
     @Override
     public int compare(Bank b1, Bank b2) {
-        return b1.getCredentials().compareTo(b2.getCredentials());
+        return b1.getPasscode().compareTo(b2.getPasscode());
     }
 }
