e3c1d525bcbbf6c883140d459abb7bf035105834
razzienyt <timkamotycena@gmail.com>
2025-03-16 23:36:36 +0800

    in

--- Accounts/FundTransfer.java
+++ Accounts/FundTransfer.java
@@ -1,40 +1,40 @@
 package Accounts;
 
 import Bank.Bank;
+import account.Account;
 
-public interface FundTransfer{
-    /**
-     * Cross-transferring of money from one account of one bank, and another account of another bank.
-     * Each transfer may require an additional process fee, which is directly deducted
-     * from the source account's account balance.
-     * <br><br>
-     * Cannot proceed if one of the following is true:
-     * <ul>
-     *     <li>Insufficient balance from source account.</li>
-     *     <li>Account types are incompatible for fund transfer purposes.</li>
-     * </ul>
-     * @param bank Bank.Bank ID of the recepient's account.
-     * @param account Recipient's account number.
-     * @param amount Amount of money to be transferred.
-     * @throws IllegalAccountType This error is thrown depending on the rules set upon. Generally
-     * occurs when fund transferring from an incompatible account type.
-     */
-    public boolean transfer(Bank bank, Account account, double amount) throws IllegalAccountType;
+public interface FundTransfer {
 
     /**
-     * Transfer money from one account on the same bank, using the
-     * recepient's account number.
-     * <br><br>
-     * Cannot proceed if one of the following is true:
-     * <ul>
-     *     <li>Insufficient balance from source account.</li>
-     *     <li>Recepient account does not exist.</li>
-     *     <li>Recepient account is from another bank.</li>
-     * </ul>
-     * @param account Accounts.Account number of the recepient.
-     * @param amount Amount of money to be transferred.
-     * @throws IllegalAccountType This error is thrown depending on the rules set upon. Generally occurs
-     * when fund transferring from an incompatible account type.
+     * Transfer money between different banks.
+     * 
+     * Each transfer applies a processing fee based on the source account's bank.
+     * 
+     * Cannot proceed if:
+     * - Insufficient balance from the source account.
+     * - Account types are incompatible for fund transfer purposes.
+     * 
+     * @param sourceAccount The sender's account.
+     * @param recipientBank The recipient's bank.
+     * @param recipientAccount The recipient's account.
+     * @param amount The amount to be transferred.
+     * @return `true` if the transaction is successful, otherwise `false`.
+     * @throws IllegalAccountType If fund transfer occurs between incompatible account types.
      */
-    public boolean transfer(Account account, double amount) throws IllegalAccountType;
+    public boolean transfer(Account sourceAccount, Bank recipientBank, Account recipientAccount, double amount) throws IllegalAccountType;
+
+    /**
+     * Transfer money within the same bank.
+     * 
+     * Cannot proceed if:
+     * - Insufficient balance from the source account.
+     * - Recipient account does not exist.
+     * 
+     * @param sourceAccount The sender's account.
+     * @param recipientAccount The recipient's account.
+     * @param amount The amount to be transferred.
+     * @return `true` if the transaction is successful, otherwise `false`.
+     * @throws IllegalAccountType If fund transfer occurs between incompatible account types.
+     */
+    public boolean transfer(Account sourceAccount, Account recipientAccount, double amount) throws IllegalAccountType;
 }
